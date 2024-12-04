25e8d7f6a0b68eed2bc712476b96f3cbb57884e3
CTJoriginal <ctjoriginal@gmail.com>
2024-12-04 23:44:47 +0100

    Day 4 completed with way too much time spent on it...

--- 2024/4/Day_4.py
+++ 2024/4/Day_4.py
@@ -1,64 +1,84 @@
 import numpy as np
-def Search(index, array, cols, rows, paintData):
+
+def SearchXMAS(index, array, cols, rows, word = "XMAS"):
+    directions = np.array([
+        #X, Y
+        [0, 1],  #N 
+        [1, 0],  #E
+        [0, -1], #S
+        [-1, 0], #W
+        [1, 1],  #NW
+        [1, -1], #SE
+        [-1, -1],#SW
+        [-1, 1]])  #NW
+        
+    count = 0
+    for direction in directions:
+        Found = True
+        for i in range(len(word)):
+            sx, sy = index + i * direction
+
+            if sx > cols - 1 or sy > rows - 1 or sx < 0 or sy < 0:
+                Found = False
+                break
+            
+            if array[sx][sy] != word[i]:
+                Found = False
+                break
+            
+            # If this passes, the word was matched!
+        if Found:
+            count += 1
+
+    return count
+        
+
+
+data = open("2024\\4\\input.txt", "r").readlines()
+data = np.array([[char for char in string if char != "\n"] for string in data])
+
+rows, cols = data.shape
+
+Sum = 0
+for x in range(rows):
+    for y in range(cols):
+        if data[x, y] == "X":
+            Sum += SearchXMAS(np.array([x, y]), data, cols, rows)
+
+print("Part 1: ", Sum)
+
+
+### PART 2 ###
+# Find X-MAS instead of XMAS
+
+def SearchX_MAS(index, array):
     mask = [['M', '.', 'S'],
             ['.', 'A', '.'],
             ['M', '.', 'S']]
     
     x, y = index
 
-    element = np.array(array[x-1 : x+2, y-1: y+2])
+    element = np.array(array[x-1 : x+2, y-1: y+2]) # COPY SLICE OF ARRAY SO ORIGINAL IS NOT MODIFIED!
     element[0, 1] = "."
     element[1, 0] = "."
     element[2, 1] = "."
     element[1, 2] = "."
 
-    print(element)
-
-
-    for i in range(5):
+    for i in range(4):
         if np.array_equal(mask, element):
-            paintData[x, y] =     "■"
-            paintData[x-1, y-1] = "■"
-            paintData[x+1, y+1] = "■"
-            paintData[x-1, y+1] = "■"
-            paintData[x+1, y-1] = "■"
-
-            return True, paintData
+            return True
         else:
             mask = np.rot90(mask)
 
-    return False, paintData
+    return False
 
-data = open("TestInput.txt", "r").readlines()
-data = np.array([[char for char in string if char != "\n"] for string in data])
-print(data)
 
 rows, cols = data.shape
 
-
-
 Sum = 0
 PaintedData = np.array(data)
-for x in range(rows):
-    for y in range(cols):
-        if data[x, y] == "A":
-            res, PaintedData = Search(np.array([x, y]), data, cols, rows, PaintedData)
-            Sum += res
-            print(x, y)
-
-            print("   " + " ".join([str(i) for i in range(cols)]))
-            # Print each row with its index
-            # for i, row in enumerate(PaintedData):
-            #     print(f"{i}  " + " ".join(row)) 
-            # print()      
-            # print()      
-
-
+for x in range(1, rows-1):
+    for y in range(1, cols-1):
+        Sum += SearchX_MAS(np.array([x, y]), data)
+        
 print("Part 2: ", Sum)
-print(data)
-
-# Print column indices
-print("   " + " ".join([str(i) for i in range(cols)]))
-# Print each row with its index
-for i, row in enumerate(PaintedData):
-    print(f"{i}  " + " ".join(row))
