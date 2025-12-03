809bcc11002a07a5415f872b160aaf675c7ae3bf
CTJoriginal <cene.trcek@gmail.com>
2025-12-03 22:18:04 +0100

    Super clean solution for both parts of day 3!

--- 2025/3/Puzzle.py
+++ 2025/3/Puzzle.py
@@ -1,37 +1,32 @@
 import re
-import numpy as np
 import time
 import numpy as np
 
-solution_1 = 0
-solution_2 = 0
+solution_1 = 0, solution_2 = 0
 
- 
+# Num batteries should be one less than actual number of digits in answer
+def yoltage(bank, solution = [], num_batteries = 2):
+    index = np.argmax(bank[:-num_batteries or None]) # usage of the "or None" -> arr[:-0] == [] vs arr[:None] == arr
+    solution.append(bank[index])
+    
+    if num_batteries > 0:
+        return yoltage(bank[index+1:], solution, num_batteries - 1)
+    else:
+        return int("".join(map(str, solution)))
     
 st = time.time()
-
 with open("3/input.txt", "r") as inpt:
     banks = np.array([[int(y) for y in i] for i in re.findall(r"\d+", inpt.read())])
 
-
 for bank in banks:
-    jolt_index1 = np.argmax(bank)
-    if jolt_index1 < len(bank) - 1:
-        jolt_index2 = np.argmax(bank[jolt_index1 + 1:]) + jolt_index1 + 1
-    elif jolt_index1 == len(bank) - 1:
-        jolt_index2 = np.argmax(bank[:jolt_index1])
-
-    max_joltage = int(f"{bank[min(jolt_index1, jolt_index2)]}{bank[max(jolt_index1, jolt_index2)]}")
-    solution_1 += max_joltage
+    solution_1 += yoltage(bank, [], 1)
+    solution_2 += yoltage(bank, [], 11)
     
-    # print(bank, end=" ")
-    # print(f"{jolt_index1:2} - {jolt_index2:2} -> {max_joltage}")
-
 t = time.time() - st
 
 print("\n", "=" * 30, sep="")
 print(f"Part 1: {solution_1} - {solution_1 == 17074}")
-print(f"Part 2: {solution_2} - {solution_2 == 3121910778619}")
+print(f"Part 2: {solution_2} - {solution_2 == 169512729575727}")
 print("=" * 30,  sep="")
 print(f"t: {t:.4f} \n")
 
