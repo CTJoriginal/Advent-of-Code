555d42cf63bd118471627df407a02312b6ce8268
CTJoriginal <cene.trcek@gmail.com>
2025-12-08 00:03:30 +0100

    Day 7

--- 2025/7/Puzzle.py
+++ 2025/7/Puzzle.py
@@ -1,7 +1,4 @@
 import time
-import re
-from functools import reduce
-# import numpy as np
 from functools import cache
 import copy
 
@@ -10,64 +7,19 @@ solution_2 = 0
 
 st = time.time()
 
-def to_tuple(arr):
-    return tuple(map(tuple, arr))
-
-def particle(manifold_map, y):
-    if y == len(manifold_map):
-        # print(manifold_map, "\n")
-        return 1
-    
-    timelines = 0
-    for beam, el in enumerate(manifold_map[y-1]):
-        if el not in ("S", "|"):
-            continue
-        
-        if manifold_map[y][beam] == "^":
-            left_path = copy.deepcopy(manifold_map)
-            right_path = copy.deepcopy(manifold_map)
-            
-            left_path[y][beam - 1] = "|"
-            right_path[y][beam + 1] = "|"
-            
-            # timelines += 1 # This timeline split
-            timelines += particle(left_path, y+1) # Get number of splits on Left path
-            timelines += particle(right_path, y+1) # Left path
-
-        else:
-            path = copy.deepcopy(manifold_map)
-            path[y][beam] = "|"
-            timelines += particle(path, y+1)
-
-    return timelines
-
 @cache
-def particle_cached(manifold_map_tup, y):
+def particle_cached(manifold_map_tup, y, x):
     if y == len(manifold_map_tup):
-        # print(manifold_map, "\n")
         return 1
     
-    manifold_map = [list(row) for row in manifold_map_tup]
-    
     timelines = 0
-    for beam, el in enumerate(manifold_map[y-1]):
-        if el not in ("S", "|"):
-            continue
-        
-        if manifold_map[y][beam] == "^":
-            left_path = copy.deepcopy(manifold_map)
-            right_path = copy.deepcopy(manifold_map)
-            
-            left_path[y][beam - 1] = "|"
-            right_path[y][beam + 1] = "|"
-            
-            timelines += particle_cached(to_tuple(left_path), y+1) # Get number of splits on Left path
-            timelines += particle_cached(to_tuple(right_path), y+1) # Left path
+    
+    if manifold_map_tup[y][x] == "^":        
+        timelines += particle_cached(manifold_map_tup, y+1, x - 1) # Left path
+        timelines += particle_cached(manifold_map_tup, y+1, x + 1) # Right path
 
-        else:
-            path = copy.deepcopy(manifold_map)
-            path[y][beam] = "|"
-            timelines += particle_cached(to_tuple(path), y+1)
+    else:
+        timelines += particle_cached(manifold_map_tup, y+1, x)
 
     return timelines
 
@@ -76,15 +28,8 @@ with open("7/input.txt", "r") as inpt:
     manifold = [[ch for ch in line if ch != "\n"] for line in inpt.readlines()]
     manifold_copy = copy.deepcopy(manifold)    
     
-    # manifold = [line.replace("\n", "") for line in inpt.readlines()]
 
-print()
-print(manifold)
-
-#          [x, y - 1]
-# [x - 1, y] [x, y] [x + 1, y]
-#          [x, y + 1]
-
+# Part 1
 for y in range(1, len(manifold)):
     for beam in [i for i, el in enumerate(manifold[y-1]) if (el == "S" or el == "|")]:
         if manifold[y][beam] == "^":
@@ -95,15 +40,13 @@ for y in range(1, len(manifold)):
             manifold[y][beam] = "|"
     # print(f"{y:3}", manifold[y])
 
-# solution_2 = particle(manifold_copy, 1)
-solution_2 = particle_cached(to_tuple(manifold_copy), 1)
-print(particle_cached.cache_info())
+# Part 2
+solution_2 = particle_cached(tuple(map(tuple, manifold_copy)), 1, len(manifold_copy[0]) // 2)
 
 t = time.time() - st
 print("\n", "=" * 30, sep="")
 print(f"Part 1: {solution_1} - {solution_1 == 21}")
 print(f"Part 2: {solution_2} - {solution_2 == 40}")
-# print(solution)
 print("=" * 30,  sep="")
 print(f"t: {t:.4f} \n")
 
