5959c7e14ca7a24e82b6d268ecfa6c9515cb5a73
CTJoriginal <ctjoriginal@gmail.com>
2025-12-11 01:51:35 +0100

    Part 2 using ILP vip

--- 2025/10/Puzzle.py
+++ 2025/10/Puzzle.py
@@ -1,6 +1,8 @@
 import time
 import numpy as np
 import re
+import pulp
+
 
 solution_1 = []
 solution_2 = 0
@@ -34,37 +36,61 @@ with open("10/input.txt") as inpt:
         
         machine.yoltages = np.fromiter((int(n) for n in pattern_digits.findall(match[2])), int)
         machines.append(machine)
-
-def findCombo(machine, presses, max_presses, lights):
-    for button in machine.buttons:
-        new_lights = np.bitwise_xor(button, lights)
-        if np.array_equal(machine.lights, new_lights):
-            return presses
-        if presses < max_presses:
-            findCombo(machine, presses + 1, max_presses, new_lights)
-             
-    return -1
         
 ## LOGIC
 print("Computing...")
-for machine in machines:
-    candidates = []
-    for button in machine.buttons:
-        res = findCombo(machine, 1, 10, np.zeros(len(machine.lights), dtype = int))
-        if res > 0:
-            candidates.append(res)
-            break
-    
-    if candidates:
-        solution_1.append(min(candidates))
-    else:
-        print(f"SOLUTION NOT FOND FOR MACHINE {machine.lights}")
 
+from collections import deque
+
+def bfs(machine):
+    queue = deque([(np.zeros(len(machine.lights), int), 0)]) # state
+    visited = set()
+    
+    while queue:
+        curr, depth = queue.popleft()
+        visited.add(tuple(curr))
+        
+        for but in machine.buttons:
+            new_panel = np.bitwise_xor(curr, but)
+            
+            if np.array_equal(new_panel, machine.lights):
+                # print(visited)
+                return depth+1
+            
+            if tuple(new_panel) not in visited: # Check that node was not yet visited
+                queue.append((new_panel, depth + 1))
+
+
+    print(f"Solution not found for machine {machine.lights}")
+    return 0
+
+# for machine in machines:
+#     solution_1.append(bfs(machine))
+
+for machine in machines:
+    model = pulp.LpProblem("Yoltages", pulp.LpMinimize)
+    variables = [pulp.LpVariable(f"button_{i}", 0, cat="Integer") for i in range(len(machine.buttons))] # Variables that we optimise for
+
+    model += pulp.lpSum(variables), "Presses" # Solution we are searching for (minimal sum of variables)
+    
+    for i in range(len(machine.yoltages)):
+        print(i)
+     #  constraint = pulp.lpSum(   x_vars[j] for j in range(m)                    if i in buttons[j])            
+        constraint = pulp.lpSum(variables[j] for j in range(len(machine.buttons)) if i in machine.buttons[j])
+        model += constraint == machine.yoltages[i], f"Sum {i}"
+    
+    model.solve()
+    if pulp.LpStatus[model.status] == "Optimal":
+        solution_2 += sum(int(pulp.value(var)) for var in variables)
+    else: 
+        print("SOLUTION NOT FOUND!")
+    print( sum(int(pulp.value(var)) for var in variables))
+    
+    
 time = time.time() - st
 
 print("\n", "=" * 50, sep="")
-print(f"Part 1: {solution_1} - {np.sum(solution_1) == 7}")
+print(f"Part 1: {sum(solution_1)} - {np.sum(solution_1) == 7}")
 print(f"Part 2: {solution_2} - {solution_2 == 85513235135}")
 print("=" * 50,  sep="")
 print(f"t: {time:.4f} \n")
-
